11ef562e2b5a9efdadf80e62e6ebf6bc4dd47fdd
unknown <sholomitkyR@yandex.by>
2021-08-15 20:54:08 +0300

    feat: create main function, number 1-999

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+function toReadable (number) {
+    const arr = ['','one','two','three','four', 'five','six','seven','eight','nine','ten',
+    'eleven','twelve', 'thirteen ','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    const arrDozens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+    if (number < 20) {return arr[number]}
+    else if (number >= 20 && number < 100) {return `${arrDozens[Math.floor(number / 10)]} ${arr[number % 10]}`}
+    else if (number > 99) { return console.log(`${arr[Math.floor(number / 100)]} hundred ${arrDozens[Math.floor((number % 100) / 10)]} ${arr[number % 10]}`)}
 }
+
+toReadable(72)
